fcd16c47acbfe8a5094c152d73580b4b30df8f4b
Anton1o-I <a.iniguez21@gmail.com>
2019-02-05 15:48:24 -0700

    began work on the prior builder

---
+++ thompson_sampling/prior.py
@@ -0,0 +1,11 @@
+
+#TODO build out functionality to add priors
+class Prior:
+    def __init__(self, distribution):
+        self.distribution = distribution
+
+    def _gamma_prior(self, mean, interval):
+        pass
+    def _exponential_prior(self, mean, inteval):
+        pass
+    def add_prior(self, label:str, mean: int = None, interval:int = None):
