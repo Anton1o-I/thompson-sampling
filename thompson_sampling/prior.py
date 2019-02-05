eade38f5b6abf7b678340c600a236fee10e0a2c6
Anton1o-I <a.iniguez21@gmail.com>
2019-02-05 15:49:10 -0700

    edit to pass

--- thompson_sampling/prior.py
+++ thompson_sampling/prior.py
@@ -1,11 +1,15 @@
 
-#TODO build out functionality to add priors
+# TODO build out functionality to add priors
 class Prior:
     def __init__(self, distribution):
         self.distribution = distribution
 
     def _gamma_prior(self, mean, interval):
         pass
+
     def _exponential_prior(self, mean, inteval):
         pass
-    def add_prior(self, label:str, mean: int = None, interval:int = None):
+
+    def add_prior(self, label: str, mean: int = None, interval: int = None):
+        pass
+
