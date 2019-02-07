971abc1224b6eb6a3b790bb29b199df021def870
Anton1o-I <a.iniguez21@gmail.com>
2019-02-07 16:13:20 -0700

    edited version number

--- setup.py
+++ setup.py
@@ -2,19 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="thompson-sampling",
-    version="0.0.0",
+    version="0.0.1",
     description="Thompson Sampling",
     author="Anton1o-I",
     author_email="a.iniguez21@gmail.com",
     packages=find_packages(),
     license="LICENSE.txt",
     long_description=open("README.md").read(),
-    install_requires=[
-        "typing",
-        "numpy",
-        "seaborn",
-        "matplotlib"
-        ],
-    tests_require = ["pytest"],
-    )
-    
+    install_requires=["typing", "numpy", "seaborn", "matplotlib", "pandas"],
+    tests_require=["pytest"],
+)
+
