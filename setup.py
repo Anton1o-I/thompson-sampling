624a11c34195a326f416dbe1c098e14c3ebffd45
Anton1o-I <a.iniguez21@gmail.com>
2020-02-04 08:27:56 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="thompson-sampling",
-    version="0.0.3",
+    version="0.0.4",
     description="Thompson Sampling",
     author="Anton1o-I",
     author_email="a.iniguez21@gmail.com",
     packages=find_packages(),
     license="LICENSE.txt",
     url="https://github.com/Anton1o-I/thompson-sampling",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=["typing", "numpy", "seaborn", "matplotlib", "pandas"],
     tests_require=["pytest"],
